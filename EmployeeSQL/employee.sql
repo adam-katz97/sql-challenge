3569789b17bea623561f884639ac57cd3d6466f2
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-10-29 13:04:12 -0500

    makes tables for the departments, department employee numbers, salaries, job titles, and department managers.

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -1,4 +1,28 @@
 create table departments(
-	dept_no varchar not NULL Primary Key, 
+	dept_no varchar not Null Primary Key, 
 	dept_name varchar not NULL
+);
+create table salaries(
+emp_no INT not null primary key,
+	salary INT not null
+);
+
+
+create table dept_emp(
+	emp_no Integer not Null,
+	Foreign Key (emp_no) References salaries(emp_no),
+	dept_no varchar not Null, 
+	Foreign Key (dept_no) References departments(dept_no)
+	
+);
+create table titles(
+title_id varchar not null primary key,
+	title varchar not null
+);
+
+create table dept_manager(
+dept_no varchar not null,
+	foreign key (dept_no) references departments(dept_no),
+	emp_no int not null,
+	foreign key (emp_no) references salaries(emp_no)
 );
--- EmployeeSQL/table.sql
+++ EmployeeSQL/table.sql
@@ -1,1 +1,5 @@
 select * from departments;
+select * from salaries;
+select * from dept_emp;
+select * from titles;
+select * from dept_manager;
