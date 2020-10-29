74385035d7a5a63a2320d62c3b0eecdc29d56fce
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-10-29 15:05:03 -0500

    finds the first and last names of department managers as well as the department number, employee number and department name.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -2,7 +2,15 @@ select employees.emp_no, employees.last_name, employees.first_name, employees.se
 join salaries 
 on employees.emp_no=salaries.emp_no;
 
-
-
 select first_name, last_name, hire_date from employees
 where extract(year from hire_date) = 1986; 
+
+select dept_manager.dept_no, departments.dept_name, 
+dept_manager.emp_no, employees.first_name, employees.last_name 
+from dept_manager
+join dept_emp
+on (dept_manager.emp_no = dept_emp.emp_no) and (dept_manager.dept_no = dept_emp.dept_no)
+join employees
+on dept_emp.emp_no = employees.emp_no
+join departments
+on dept_emp.dept_no = departments.dept_no;
