fff878255b1758d39b4c608f7876f14d3cca28fa
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-10-29 13:29:27 -0500

    renames sql files to better indicate what they are meant for and makes first query.

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,3 @@
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary from employees
+join salaries 
+on employees.emp_no=salaries.emp_no;
--- EmployeeSQL/table.sql
+++
@@ -1,5 +0,0 @@
-select * from departments;
-select * from salaries;
-select * from dept_emp;
-select * from titles;
-select * from dept_manager;
