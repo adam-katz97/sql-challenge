4d7b8e74058cba326a5937a50ad0a45a0c06cd37
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-10-29 14:22:12 -0500

    finds the first and last names of all employees hired in 1986.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,3 +1,8 @@
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary from employees
 join salaries 
 on employees.emp_no=salaries.emp_no;
+
+
+
+select first_name, last_name, hire_date from employees
+where extract(year from hire_date) = 1986; 
