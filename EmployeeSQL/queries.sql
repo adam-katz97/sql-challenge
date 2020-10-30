cc2a37a06829dd3aaa607015ea84bd2e4a62d74c
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-10-30 11:13:47 -0500

    Lists the first and last names of all employees in the sales department as well as their employee number.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -24,3 +24,10 @@ on departments.dept_no = dept_emp.dept_no;
 
 select first_name, last_name, sex from employees 
 where (first_name = 'Hercules' ) and (last_name like 'B%');
+
+select dept_emp.emp_no, employees.first_name, employees.last_name, departments.dept_name from employees
+join dept_emp
+on dept_emp.emp_no = employees.emp_no 
+join departments 
+on departments.dept_no = dept_emp.dept_no
+where departments.dept_name = 'Sales';
